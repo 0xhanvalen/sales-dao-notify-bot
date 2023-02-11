6a0b7c679b47653fb9bb8ae07c9db1a69a5779b3
Han Valen <103603290+0xhanvalen@users.noreply.github.com>
2023-02-11 10:29:20 -0800

    Update server.js

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ const createServer = (client) => {
 
   app.use('/notify', auth, NOTIFY_ROUTER);
 
-  app.listen(process.env.PORT || 5000, () =>
+  app.listen(`0.0.0.0:${process.env.PORT}` || 5000, () =>
     console.log(`Server Listening on port ${process.env.PORT || 5000}..`)
   );
 };
